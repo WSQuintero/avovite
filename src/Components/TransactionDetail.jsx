0e9bc8f27792036f6c0d40b76d738f4ae626909a
Juan Diego Garzon Ovalle <gaova777@utp.edu.co>
2023-07-25 22:35:59 -0500

    show transfer data.[1h]

--- src/Components/TransactionDetail.jsx
+++ src/Components/TransactionDetail.jsx
@@ -52,17 +52,38 @@ function TransactionDetail() {
   return (
     <Box
       position="absolute"
-      height="100%"
-      width="100%"
+      
       display="flex"
       flexDirection="column"
-      sx={{
-        backgroundColor: "white",
-      }}
+      sx={(theme)=>({
+       
+        backgroundColor:'white',
+        height:"150%",
+            width:"100%",
+        [theme.breakpoints.up('lg')]:{
+          backgroundColor:'#FFFFFF',
+          
+          height:'2000vh',
+          width:'200vh',
+         marginLeft:-20,
+          marginTop:10,
+        }
+    })}
     >
       <Header />
 
-      <Box width="100%" height={40} bgcolor="#67AA36">
+      <Box width="100%" height={40} bgcolor="#67AA36"
+      sx={(theme)=>({
+        display:'flex',
+        width:'100%',
+        height:40,
+        marginTop:7,
+        [theme.breakpoints.up('lg')]:{
+          display:'none'
+
+        }
+      })}
+      >
         <Button
           onClick={() => handleCloseForm()}
           sx={{
@@ -77,58 +98,200 @@ function TransactionDetail() {
       </Box>
 
       {/* the filter of image */}
-
-      <Grid display="flex" flexDirection="column">
+      <Typography variant='h3' sx={(theme)=>({
+              display:'none',
+              [theme.breakpoints.up('lg')]:{
+                display:'flex',
+                marginLeft:15,
+                marginTop:10,
+                
+              }
+          
+        })}>{filterBanck.banco}</Typography>
+      <Grid display="flex" flexDirection="column"
+      
+      sx={(theme)=>({
+        justifyContent:'center',
+        alignItems:'center',
+        paddingX:0,
+        paddingY:4,
+        [theme.breakpoints.up('lg')]:{
+          border:1,
+          borderColor:'primary.main',
+          marginLeft:50,
+          marginTop:5,
+          paddingBottom:10,
+         borderRadius:3,
+          width:500,
+          height:500,
+        }
+      })}>
         <Grid
-          flexGrow={1}
-          paddingX={4}
-          paddingY={4}
+          
           display="flex"
           flexDirection="column"
           gap={2}
+          sx={(theme)=>({
+            paddingX:4,
+            paddingY:4,
+            alignItems:'center',
+            justifyContent:'center',
+            width:'100%',
+            height:'100%',
+            [theme.breakpoints.up('lg')]:{
+              marginTop:5,
+              width:'100%',
+              height:'100%',
+            }
+          })}
         >
           <Typography variant="h2" color="primary.main" fontWeight={549}>
             Datos de trasferencia{" "}
           </Typography>
 
-          <Grid>
-            <Typography color="primary.main">
+          <Grid  sx={(theme)=>({
+            
+            [theme.breakpoints.up('lg')]:{
+              width:'100%',
+              marginLeft:10,
+            }
+          })}>
+            <Typography color="primary.main"  sx={(theme)=>({
+            
+            [theme.breakpoints.up('lg')]:{
+              width:'100%',
+              fontSize:18,
+            }
+          })}>
               Fecha y hora de transacción
             </Typography>
-            <Typography color="text.cards">
+            <Typography color="text.cards"  sx={(theme)=>({
+              
+            [theme.breakpoints.up('lg')]:{
+              width:'100%',
+              fontSize:18,
+            }
+          })}>
               Transacción {filterBanck.banco}
             </Typography>
-            <Typography color="text.cards">2024/06/07 - 13:27:31 </Typography>
+            <Typography color="text.cards"  sx={(theme)=>({
+            
+            [theme.breakpoints.up('lg')]:{
+              width:'100%',
+              fontSize:18,
+            }
+          })}>2024/06/07 - 13:27:31 </Typography>
           </Grid>
 
-          <Box width="100%" height={1} bgcolor="text.disabled" border={1}></Box>
+          <Box width="100%" height={1} bgcolor="text.disabled" border={1}  sx={(theme)=>({
+             display:'flex',
+            
+            [theme.breakpoints.up('lg')]:{
+             display:'none'
+              
+            }
+          })}></Box>
 
-          <Grid>
-            <Typography color="primary.main">Origen</Typography>
-            <Typography color="text.cards">
+          <Grid sx={(theme)=>({
+            
+            [theme.breakpoints.up('lg')]:{
+              width:'100%',
+              marginLeft:10,
+            }
+          })}>
+            <Typography color="primary.main" sx={(theme)=>({
+            
+            [theme.breakpoints.up('lg')]:{
+              width:'100%',
+              fontSize:18,
+            }
+          })}>Origen</Typography>
+            <Typography color="text.cards" sx={(theme)=>({
+            
+            [theme.breakpoints.up('lg')]:{
+              width:'100%',
+              fontSize:18,
+            }
+          })}>
             Cuenta Avovite: 
 
             </Typography>
-            <Typography color="text.cards">N: *********0413</Typography>
+            <Typography color="text.cards" sx={(theme)=>({
+            
+            [theme.breakpoints.up('lg')]:{
+              width:'100%',
+              fontSize:18,
+            }
+          })}>N: *********0413</Typography>
           </Grid>
-          <Box width="100%" height={1} bgcolor="text.disabled" border={1}></Box>
-          <Grid>
-            <Typography color="primary.main">
+          <Box width="100%" height={1} bgcolor="text.disabled" border={1} sx={(theme)=>({
+             display:'flex',
+            
+            [theme.breakpoints.up('lg')]:{
+             display:'none'
+              
+            }
+          })}></Box>
+          <Grid sx={(theme)=>({
+            
+            [theme.breakpoints.up('lg')]:{
+              width:'100%',
+              marginLeft:10,
+            }
+          })}>
+            <Typography color="primary.main" sx={(theme)=>({
+            
+            [theme.breakpoints.up('lg')]:{
+              width:'100%',
+              fontSize:18,
+            }
+          })}>
             Valor de transacción 
             </Typography>
-            <Typography color="text.cards">
+            <Typography color="text.cards" sx={(theme)=>({
+            
+            [theme.breakpoints.up('lg')]:{
+              width:'100%',
+              fontSize:18,
+            }
+          })}>
             $ 1.000.000  
             </Typography>
             
           </Grid>
 
-          <Box width="100%" height={1} bgcolor="text.disabled" border={1}></Box>
+          <Box width="100%" height={1} bgcolor="text.disabled" border={1} sx={(theme)=>({
+             display:'flex',
+            
+            [theme.breakpoints.up('lg')]:{
+             display:'none'
+              
+            }
+          })}></Box>
 
-          <Grid>
-            <Typography color="primary.main">
+          <Grid sx={(theme)=>({
+            
+            [theme.breakpoints.up('lg')]:{
+              width:'100%',
+              marginLeft:10,
+            }
+          })}>
+            <Typography color="primary.main" sx={(theme)=>({
+            
+            [theme.breakpoints.up('lg')]:{
+              width:'100%',
+              fontSize:18,
+            }
+          })}>
               Numero de autorización 
             </Typography>
-            <Typography color="text.cards">
+            <Typography color="text.cards" sx={(theme)=>({
+            
+            [theme.breakpoints.up('lg')]:{
+              width:'100%',
+              fontSize:18,
+            }
+          })}>
             008473
             </Typography>
             
