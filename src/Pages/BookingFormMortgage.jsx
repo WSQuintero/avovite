8ca422ac5e818957579ef55c5a41da34b3f5cf11
JCiroLo <juanciro35@gmail.com>
2023-12-29 11:35:23 -0500

    Usuario / Whitelist: Verificar en el endpoint de la sesión del usuario si pertenece a la whitelist, si pertenece quitar la sección 'Comprar vites' y poner una nueva sección llamada 'Pago por cuotas', la cual redirgirá al formulario de hipotecas con los datos previamente creados

--- src/Pages/BookingFormMortgage.jsx
+++ src/Pages/BookingFormMortgage.jsx
@@ -1,25 +1,17 @@
-import { useMemo, useState } from "react";
-import {
-  Button,
-  Container,
-  Grid,
-  Typography,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogContentText,
-  DialogActions,
-} from "@mui/material";
+import { useEffect, useMemo, useState } from "react";
+import { Button, Container, Grid, Typography, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from "@mui/material";
 import { HighlightOff as ErrorIcon, CheckCircle as CheckIcon } from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
 import Form from "../Components/Form";
 import ContractService from "../Services/contract.service";
+import useSession from "../Hooks/useSession";
 
 const BookingFormMortgage = () => {
-
   const navigate = useNavigate();
+  const [{ user }] = useSession();
   const $Contract = useMemo(() => new ContractService(), []);
   const [feedback, setFeedback] = useState({ open: false, message: "", status: "success" });
+  const [initialFormData, setInitialFormData] = useState({});
 
   const handleSubmit = async (body) => {
     const { status } = await $Contract.add(body);
@@ -36,9 +28,22 @@ const BookingFormMortgage = () => {
     setFeedback((prev) => ({ show: false, message: prev.message, status: prev.status }));
   };
 
+  useEffect(() => {
+    if (user) {
+      setInitialFormData({
+        fullname: user.fullname,
+        id_type: user.id_type,
+        id_number: user.id_number,
+        id_location_expedition: user.id_location_expedition,
+        email: user.email,
+        cellphone: user.cellphone,
+      });
+    }
+  }, [user]);
+
   return (
     <Container maxWidth="xxl" sx={{ marginY: 4, padding: 4, border: 1, borderRadius: 2, borderColor: "primary.main" }}>
-      <Form isMortgage title="Aplicación con garantía hipotecaria" onSubmit={handleSubmit} />
+      <Form isMortgage title="Aplicación con garantía hipotecaria" initialState={initialFormData} onSubmit={handleSubmit} />
 
       <Dialog open={feedback.open && feedback.status === "success"} onClose={resetFeedback}>
         <DialogTitle component={Grid} display="flex" flexDirection="column" alignItems="center">
--- src/Providers/SessionProvider.jsx
+++ src/Providers/SessionProvider.jsx
@@ -1,4 +1,5 @@
 import { createContext, useEffect, useState } from "react";
+import { CONTRACT_TYPES } from "../utilities/constants";
 
 const sessionKey = "VE9LX0FWT19FTg";
 
@@ -7,7 +8,16 @@ export const SessionContext = createContext();
 export const SessionProvider = ({ children }) => {
   const [session, setSession] = useState({ user: null, token: false });
 
-  const setUser = (value) => setSession((prev) => ({ ...prev, user: { ...value, isAdmin: () => value.rol === 1 } }));
+  const setUser = (value) =>
+    setSession((prev) => ({
+      ...prev,
+      user: {
+        ...value,
+        isAdmin: () => value.rol === 1,
+        isWhitelisted: () =>
+          value.approvedlist === 1 ? CONTRACT_TYPES.mortgage : value.approvedlist === 2 ? CONTRACT_TYPES.standard : null,
+      },
+    }));
 
   const setToken = (value) => setSession((prev) => ({ ...prev, token: value }));
 
