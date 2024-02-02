e20e312ad2926ed2b6e46ed0bd4d7dd62d99798b
Juan Pablo <jhondoetk8@outlook.com>
2024-02-01 20:21:59 -0500

    Add update docs and Withdrawal

--- src/Pages/Transactions.jsx
+++ src/Pages/Transactions.jsx
@@ -1,7 +1,21 @@
 import { useMemo, useState, useEffect } from "react";
-import { Typography, Container, Stack, Box, Button } from "@mui/material";
+import { 
+  Typography, 
+  Container, 
+  Stack, 
+  Box,
+  Grid,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogActions,
+  DialogContent,
+  DialogContentText
+} from "@mui/material";
+import { LoadingButton } from "@mui/lab";
 import PageWrapper from "../Components/PageWrapper";
 import dayjs from "dayjs";
+import useConfig from "../Hooks/useConfig";
 import { AvoviteWhiteIcon } from "../Components/Icons";
 import { formatCurrency } from "../utilities";
 import useSession from "../Hooks/useSession";
@@ -10,13 +24,19 @@ import useAsyncEffect from "../Hooks/useAsyncEffect";
 import EnhancedTable from "../Components/EnhancedTable";
 import { TRANSACTION_TYPES } from "../utilities/constants";
 import { useNavigate } from 'react-router-dom';
+import { useSnackbar } from "notistack";
 
 function Transactions() {
   const navigate = useNavigate();
-  
+  const { enqueueSnackbar } = useSnackbar();
   const [{ user, token }] = useSession();
   const [rows, setRows] = useState([]);
-  const [loading, setLoading] = useState({ fetching: true });
+  const [modal, setModal] = useState([]);
+  const [filesUser, setFilesUser] = useState({cedula: null, certificado: null});
+  const [withdrawalMovId, setWithdrawalMovId] = useState(null);
+
+  const [{ loading }, { setLoading }] = useConfig();
+  const [loadingTransactions, setLoadingTransactions] = useState({ fetching: true });
   const $Movement = useMemo(() => (token ? new MovementService(token) : null), [token]);
   const columns = useMemo(
     () => [
@@ -47,27 +67,38 @@ function Transactions() {
       {
         id: "id",
         label: "",
-        format: (value) => (
-          <Stack direction="row" spacing={1}>
+        format: (mov, data) => {
+          return (<Stack direction="row" spacing={1}>
             <Button disabled size="small" variant="contained">
               Ver detalles
             </Button>
-          </Stack>
-        ),
+            
+            {data.withdrawal==0&&(<Button size="small" onClick={()=>{ 
+              setWithdrawalMovId(mov);
+              setModal("modal-withdrawal"); 
+            }} variant="contained">
+              Retirar
+            </Button>)}
+          </Stack>);
+        },
       },
     ],
     []
   );
 
+  const loadMovs = async()=>{
+    const { status, data } = await $Movement.get();
+
+    if (status) {
+      setRows(data.data);
+    }
+
+    setLoadingTransactions((prev) => ({ ...prev, fetching: false }));
+  };
+
   useAsyncEffect(async () => {
     if ($Movement) {
-      const { status, data } = await $Movement.get();
-
-      if (status) {
-        setRows(data.data);
-      }
-
-      setLoading((prev) => ({ ...prev, fetching: false }));
+      loadMovs();
     }
   }, [$Movement]);
 
@@ -78,6 +109,59 @@ function Transactions() {
       }
     }
   }, [user]);
+
+  const cancelWithdrawalMov = async ()=>{
+    setModal("");
+    setWithdrawalMovId(null);
+    setFilesUser({cedula: null, certificado: null});
+  };
+
+  const withdrawalMov = async ()=>{
+    setLoading(true);
+    
+    await $Movement.withdrawal(withdrawalMovId);
+    
+    await loadMovs();
+    
+    setLoading(false);
+
+    enqueueSnackbar(`Se ha generado el retiro correctamente.`, {
+      variant: "success",
+    });
+
+    cancelWithdrawalMov();
+  };
+
+  const onImport = async (file, type) => {
+    setModal("");
+
+    let alter = filesUser;
+    alter[type] = file;
+
+    setFilesUser(alter);
+
+    setTimeout(()=>setModal("modal-update-payment-docs"),1);
+  };
+
+  const updateDocsUser = async ()=>{
+    setLoading(true);
+
+    const body = new FormData();
+
+    body.append("info", [filesUser.cedula, filesUser.certificado]);
+
+    await $Movement.changeInformationBank(body);
+    
+    await loadMovs();
+    
+    setLoading(false);
+
+    enqueueSnackbar(`Se han cargado los documentos correctamente.`, {
+      variant: "success",
+    });
+
+    cancelWithdrawalMov();
+  };
 
   return (
     <PageWrapper>
@@ -90,8 +174,94 @@ function Transactions() {
             Transacciones
           </Typography>
         </Stack>
-        <EnhancedTable loading={loading.fetching} headCells={columns} rows={rows} />
+        <EnhancedTable loading={loadingTransactions.fetching} headCells={columns} rows={rows} />
       </Container>
+
+      <Dialog open={modal === "modal-update-payment-docs"} onClose={() => cancelWithdrawalMov()}  maxWidth="md" fullWidth>
+        <DialogTitle color="primary.main">Actualizar datos</DialogTitle>
+          <DialogContent>
+            <DialogContentText>Adjunta PDF de tus documentos</DialogContentText>
+            <br />
+
+            <Grid display="flex" gap={1}>
+              <Box position="relative">
+                <LoadingButton loading={loading.importing} variant={(filesUser.cedula?"contained":"")} size="small">
+                  {(filesUser.cedula?"Cambiar cédula cargada":"Anexar Cédula")}
+                </LoadingButton>
+
+                <input
+                  type="file"
+                  style={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    zIndex: 1,
+                    width: "100%",
+                    height: "100%",
+                    cursor: "pointer",
+                    aspectRatio: 1,
+                    opacity: 0,
+                  }}
+                  onChange={({ target }) => onImport(target.files[0], "cedula")}
+                />
+            </Box>
+              
+            <Box position="relative">
+                <LoadingButton loading={loading.importing} variant={(filesUser.certificado?"contained":"")} size="small">
+                  {(filesUser.certificado?"Cambiar certificación bancaria cargada":"Anexar certificación bancaria")}
+                </LoadingButton>
+                <input
+                  type="file"
+                  style={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    zIndex: 1,
+                    width: "100%",
+                    height: "100%",
+                    cursor: "pointer",
+                    aspectRatio: 1,
+                    opacity: 0,
+                  }}
+                  onChange={({ target }) => onImport(target.files[0], "certificado")}
+                />
+            </Box>
+          </Grid>
+        </DialogContent>
+        <DialogActions>
+          <Button variant={(filesUser.cedula&&filesUser.certificado?"contained":"")} onClick={() => {
+              if(filesUser.cedula&&filesUser.certificado){
+                updateDocsUser();
+              }
+            }}>
+            Actualizar
+          </Button>
+          {" "}
+          <Button variant="warning" onClick={()=>cancelWithdrawalMov()}>
+            Cancelar
+          </Button>
+        </DialogActions>
+      </Dialog>
+      
+      <Dialog open={modal === "modal-withdrawal"} onClose={() => cancelWithdrawalMov()}>
+        <DialogTitle color="primary.main">Retirar</DialogTitle>
+        <DialogContent>
+          <DialogContentText>¿Estás seguro que quieres eliminar este proveedor de la lista?</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="contained" onClick={() => withdrawalMov()}>
+            Sí
+          </Button>
+          {" "}
+          <Button onClick={() => setModal("modal-update-payment-docs")}>
+            Actualizar datos
+          </Button>
+          {" "}
+          <Button variant="warning" onClick={()=>cancelWithdrawalMov()}>
+            Cancelar
+          </Button>
+        </DialogActions>
+      </Dialog>
     </PageWrapper>
   );
 }
