07eb2895b9ed6613579047dcf6be596583323406
Santiago Quintero <santiagoquinteroperfil@gmail.com>
2024-02-17 22:27:58 -0500

    Add buttons to export transactions and movement

--- src/Services/movement.service.js
+++ src/Services/movement.service.js
@@ -19,4 +19,51 @@ export default class MovementService {
   async changeInformationBank(info, id) {
     return await handleCall(async () => (await this.axios.post(`${this.API_URL}/movements/changeinformationbanck/${id}`, info)).data);
   }
+  async exportByDate({ initDate, finalDate }) {
+    return await handleCall(async () => {
+      if (initDate && finalDate) {
+        // Corrección en la condición
+        try {
+          const response = await axios.get(
+            `${this.API_URL}/contract-transactional-payments/expor/xlsx/?dateat=${initDate}&dateend=${finalDate}`,
+            {
+              responseType: "blob",
+              headers: { Authorization: this.token }, // Agregar el token a las cabeceras
+            }
+          );
+          const url = window.URL.createObjectURL(new Blob([response.data]));
+          const link = document.createElement("a");
+          link.href = url;
+          link.setAttribute("download", `archivo.xlsx`);
+          document.body.appendChild(link);
+          link.click();
+          link.parentNode.removeChild(link);
+        } catch (error) {
+          console.error("Error al descargar el archivo:", error);
+        }
+      }
+    });
+  }
+  async exportByDateMovements({ initDate, finalDate }) {
+    return await handleCall(async () => {
+      if (initDate && finalDate) {
+        // Corrección en la condición
+        try {
+          const response = await axios.get(`${this.API_URL}/movements/export/xlsx/?dateat=${initDate}&dateend=${finalDate}`, {
+            responseType: "blob",
+            headers: { Authorization: this.token }, // Agregar el token a las cabeceras
+          });
+          const url = window.URL.createObjectURL(new Blob([response.data]));
+          const link = document.createElement("a");
+          link.href = url;
+          link.setAttribute("download", `archivo.xlsx`);
+          document.body.appendChild(link);
+          link.click();
+          link.parentNode.removeChild(link);
+        } catch (error) {
+          console.error("Error al descargar el archivo:", error);
+        }
+      }
+    });
+  }
 }
