1cab81a3919938b0c0568ce94a7e16f6c11004fe
Santiago Quintero <santiago@conexioncibernetica.com>
2024-03-02 12:33:49 -0500

    Implemented Sentry for error tracking and monitoring.

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import ReactDOM from "react-dom/client";
+import ReactDOM, { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import MasterProvider from "./Providers/index.jsx";
 import App from "./App.jsx";
@@ -6,8 +6,39 @@ import App from "./App.jsx";
 import "./assets/css/_default.css";
 import "react-phone-input-2/lib/material.css";
 import { GeneralContextProvider } from "./context/GeneralContext.jsx";
+import * as Sentry from "@sentry/react";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const hostname = window.location.hostname;
+const isDev = hostname === "appdev.avovite.com" || hostname === "localhost";
+
+let sentryDSN;
+if (isDev) {
+  sentryDSN = "https://770c39cef117658f85797a931ce17681@o4505030736347136.ingest.sentry.io/4506836583448576";
+} else {
+  sentryDSN = "https://2e4b2df4cad64511ea10b948531d888d@o4505030736347136.ingest.sentry.io/4506836592164864";
+}
+Sentry.init({
+  dsn: sentryDSN,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /\/api\/v1$/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
   <GeneralContextProvider>
     <MasterProvider>
       <BrowserRouter>
