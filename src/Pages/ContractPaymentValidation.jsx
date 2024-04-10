1d436b7c7d479d68bb16cd56fca84ecf102440f3
Santiago Quintero <santiago@conexioncibernetica.com>
2024-04-09 21:45:22 -0500

    fix problem with payment

--- src/Pages/ContractPaymentValidation.jsx
+++ src/Pages/ContractPaymentValidation.jsx
@@ -57,6 +57,35 @@ function ContractPaymentValidation() {
       // setTimeout(() => {
       //   onCloseFirstTime();
       // }, 5000);
+      const mandatory = {
+        name: "Pago del contrato pendiente",
+        description: contract.dues ? "Cuota del contrato pendiente" : "Primer pago del contrato pendiente",
+        invoice: `AV-${uuid()}`,
+        currency: "cop",
+        amount: contract.payment,
+        tax_base: "4000",
+        tax: "500",
+        tax_ico: "500",
+        country: "co",
+        lang: "es",
+      };
+
+      const aditional = {
+        extra1: null,
+        extra2: token,
+        extra3: contract.idcontrato,
+        extra4: null,
+        extra5: contract.dues,
+        confirmation: `${import.meta.env.VITE_API_URL}/contract-transactional-payments/financed`,
+        response: `${import.meta.env.VITE_APP_URL}/validation/payment`,
+      };
+
+      const handler = window.ePayco.checkout.configure({
+        key: import.meta.env.VITE_EPAYCO_PUBLIC_KEY,
+        test: TESTING_EPAYCO,
+      });
+
+      handler.open({ ...mandatory, ...aditional });
       return;
     }
     if (contract?.rejectedCounter >= 2) {
