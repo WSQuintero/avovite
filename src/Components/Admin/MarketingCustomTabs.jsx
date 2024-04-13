43b22a665b841d0adaca714d59d9a4457a1ea072
Christian Garcerant <christian@conexioncibernetica.com>
2024-04-12 19:40:10 -0500

    CREATE custom tabs to set the marketing category view

---
+++ src/Components/Admin/MarketingCustomTabs.jsx
@@ -0,0 +1,32 @@
+import { Button } from '@mui/material';
+
+const CustomTab = ({ label, selected, onClick }) => {
+  return (
+    <Button 
+      variant={selected ? 'contained' : 'text'}
+      color="primary" 
+      onClick={onClick}
+      sx={{ 
+        marginRight: 1,
+        fontSize: '14px',
+        width: '12.5%',
+        color: 'black', 
+        fontWeight:'300',
+        backgroundColor: selected ? 'rgba(201, 233, 173, 1)' : 'transparent',
+        border: 'none',
+        boxShadow: selected ? '0px .8px 0px grey' : 'none',
+        '&:hover': {
+          backgroundColor: selected ? 'rgba(201, 233, 173, 1)' : 'transparent',
+          color: selected ? 'white' : 'black',
+        },
+        '&:hover span': {
+          color: 'white',
+        },
+      }}
+    >
+      {label}
+    </Button>
+  );
+};
+
+export default CustomTab;
